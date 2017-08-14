518636bc7868e40fb392c7353b5d2a0b5783b533
jmjuanes <josemijuanes@gmail.com>
2017-08-14 17:13:00 +0200

    stattic.js: big rewrite

--- stattic.js
+++ stattic.js
@@ -2,155 +2,171 @@
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
+var url = require('url');
+var mime = require('mime');
+var utily = require('utily');
+var logty = require('logty');
 
-//Import stattic functions
-var parseUrl = require('stattic-parseurl');
-var pStat = require('stattic-pstat');
-var getMime = require('stattic-mime');
+//Server options
+var options = { folder: process.cwd(), port: 5000, cors: true, index: 'index.html' };
 
-//Create the server object
-var server = {};
-
-//Set the static folder
-server._static = '';
-
-//Set the server port
-server._port = 5000;
-
-//Set the cors
-server._cors = true;
-
-//Set the index file
-server._index = 'index.html';
-
-//Set the server error 404 message
-server._error = '<h1>Not found...</h1>';
-
-//Initialize the http client
-server._http = null;
+//Get the default error file path
+options.error = path.resolve(__dirname, './assets/error.html');
 
 //Set the default vars
-server.set = function(key, value)
+module.exports.set = function(key, value)
 {
-  //Check for static files folder
-  if(key === 'static'){ server._static = value; }
+  //Check the key
+  if(typeof options[key] === 'undefined'){ throw new Error('Unknown key "' + key + '"'); }
 
-  //Check for port
-  else if(key === 'port'){ server._port = parseInt(value); }
+  //Check the key type
+  if(typeof options[key] !== typeof value){ throw new Error('Invalid key type "' + key + '"'); }
 
-  //Check for Cross Origin
-  else if(key === 'cors'){ server._cors = value; }
+  //Save the option value
+  options[key] = value;
+};
 
-  //Check for index file
-  else if(key === 'index'){ server._index = value; }
-
-  //Default
-  else { console.warn('Stattic Error: Unknown key "' + key + '" used in set method.'); }
+//Get a value
+module.exports.get = function(key)
+{
+  //Return the value
+  return options[key];
 };
 
 //Create the server
-server.run = function()
+module.exports.listen = function(port, cb)
 {
-  //Check the statics folder
-  if(server._static === '')
-  {
-    //Show error
-    console.error('Stattic Error: no statics folder selected.');
+  //Check the callback function
+  cb = (typeof port === 'function') ? port : cb;
 
-    //Exit
-    return;
-  }
+  //Check the port value
+  port = (typeof port === 'number') ? parseInt(port) : options.port;
 
-  //Fix the stattic files folder
-  server._static = path.resolve(process.cwd(), server._static);
+  //Fix the static files folder path
+  options.folder = path.resolve(process.cwd(), options.folder);
+
+  //Fix the error file path
+  options.error = path.resolve(process.cwd(), options.error);
 
   //Initialize the server
-  server._http = http.createServer(server._Server);
+  var server = http.createServer(function(req, res)
+  {
+    //Time for make the request
+    var time_start  = Date.now();
+
+    //Check the cors option
+    if(options.cors === true)
+    {
+      //Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
+
+      //Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+      //Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    }
+
+    //Parse the request url and get only the pathname
+    var pathname = url.parse(req.url).pathname;
+
+    //Resolve to the local folder
+    var local_path = path.join(options.folder, pathname);
+
+    //Check the extension
+    if(path.extname(local_path) === '')
+    {
+      //Add the index file to the local path
+      local_path = path.join(local_path, './' + path.basename(options.index));
+    }
+
+    //Reponse finish event
+    res.on('finish', function()
+    {
+      //Get the total time in ms
+      var response_time = (Date.now() - time_start);
+
+      //Get the response code
+      var response_code = res.statusCode;
+
+      //Display the log message
+      console.log('' + response_code + ' ' + pathname + '  ' + response_time + ' ms');
+    });
+
+    //Check if the file exists in this directory
+    return utily.fs.isFile(local_path, function(error, exists)
+    {
+      //Check the error
+      if(error){ return error_page(res, 500, 'Error'); }
+
+      //Check if file does not exists
+      if(exists === false){ return error_page(res, 404, 'Not found'); }
+
+      //Write the header with the content type
+      res.writeHead(200, { 'Content-Type': mime.lookup(local_path) });
+
+      //Initialize the reader stream
+      var reader = fs.createReadStream(local_path, { encoding: 'utf8' });
+
+      //Reader data
+      reader.on('data', function(data)
+      {
+        //Write the data to the response
+        res.write(data);
+      });
+
+      //No more data -> End event
+      reader.on('end', function()
+      {
+        //End the write
+        res.end('');
+      });
+
+      //Error reading file
+      reader.on('error', function(error)
+      {
+        //Render the error page
+        return error_page(res, 500, 'Something went wrong...');
+      })
+    });
+  });
 
   //Start server
-  server._http.listen(server._port, function(){
-
+  server.listen(port, function()
+  {
     //Show the console log success
     console.log('');
     console.log('Welcome to Stattic');
-    console.log('Visit us: http://statticjs.github.io');
     console.log('');
-    console.log('Static server listening on: http://localhost:' + server._port);
-    console.log('Static files listening from: ' + server._static);
+    console.log('Static server listening on: http://localhost:' + options.port);
+    console.log('Static files listening from: ' + options.folder);
     console.log('');
 
+    //Check if the callback method is a function
+    if(typeof cb === 'function')
+    {
+      //Call the callback function
+      cb.call(null);
+    }
   });
-
 };
 
-//Serve the static files
-server._Server = function(req, res)
+//Display an error page
+var error_page = function(res, error_code, error_message)
 {
-  //Set the CORS
-  if(server._cors === true)
+  //Write the header with the content type
+  res.writeHead(error_code, { 'Content-Type': 'text/html' });
+
+  //Read the error file path
+  return fs.readFile(options.error, function(error, data)
   {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    //Check the error
+    if(error){ return res.end('<h1>Error</h1><p>' + error_message + '</p>'); }
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //Replace the html file
+    data = utily.string.format(data, { code: error_code, message: error_message });
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  }
-
-  //Initialize the http code
-  var httpc = 200;
-
-  //Get the real path to the file
-  var url = path.join(server._static, req.url);
-
-  //Parse the url
-  url = parseUrl(url);
-
-  //Check the root file
-  if(url.file === '')
-  {
-    //Update the url object
-    url = parseUrl(url.path + server._index);
-  }
-
-  //Time for make the request
-  var time = Date.now();
-
-  //Check if file exists
-  if(pStat.isFile(url.path) === true)
-  {
-    //Get the mime
-    var mime = getMime(url.ext);
-
-    //Write header
-  	res.writeHead(200, { 'Content-Type': mime });
-
-    //Get the file content
-    var cont = fs.readFileSync(url.path);
-
-    //Show the file content
-    res.end(cont);
-  }
-  else
-  {
-    //Write header for the error page
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-
-    //Show the error message
-    res.end(server._error);
-
-    //Replace the http code
-    httpc = 404;
-  }
-
-  //Count the end time
-  time = Date.now() - time;
-
-  //Show in console que request
-  console.log(httpc + ' ' + req.url + '  ' + time + ' ms');
+    //Return the error page
+    return res.end(data);
+  });
 };
-
-//Exports to node
-module.exports = server;
