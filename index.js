849b934afae5dba6ecf99c88b83e8df2cdc91629
jmjuanes <josemijuanes@gmail.com>
2018-03-05 11:33:40 +0100

    Reformat code

--- index.js
+++ index.js
@@ -1,180 +1,120 @@
-//Import dependencies
-var fs = require('fs');
-var path = require('path');
-var http = require('http');
-var url = require('url');
-var mime = require('mime');
-var utily = require('utily');
-var colors = require('colors/safe');
+let fs = require("fs");
+let path = require("path");
+let http = require("http");
+let url = require("url");
+let mime = require("mime");
+let utily = require("utily");
 
 //Server options
-var options = { folder: process.cwd(), port: 5000, cors: false, index: 'index.html' };
+let options = {folder: process.cwd(), port: 5000, cors: false, index: "index.html"};
 
 //Get the default error file path
-options.error = path.resolve(__dirname, './assets/error.html');
+options.error = path.resolve(__dirname, "./assets/error.html");
 
-//Set the default vars
-module.exports.set = function(key, value)
-{
-  //Check the key
-  if(typeof options[key] === 'undefined'){ throw new Error('Unknown key "' + key + '"'); }
-
-  //Check the key type
-  if(typeof options[key] !== typeof value){ throw new Error('Invalid key type "' + key + '"'); }
-
-  //Save the option value
-  options[key] = value;
+//Change a default option value
+module.exports.set = function (key, value) {
+    if (typeof options[key] === "undefined") {
+        throw new Error("Unknown key '" + key + "'");
+    }
+    if (typeof options[key] !== typeof value) {
+        throw new Error("Invalid key type '" + key + "'");
+    }
+    options[key] = value;
 };
 
 //Get a value
-module.exports.get = function(key)
-{
-  //Return the value
-  return options[key];
+module.exports.get = function (key) {
+    return options[key];
 };
 
 //Create the server
-module.exports.listen = function(port, cb)
-{
-  //Check the callback function
-  cb = (typeof port === 'function') ? port : cb;
+module.exports.listen = function (port, cb) {
+    //Parse the arguments
+    cb = (typeof port === "function") ? port : cb;
+    port = (typeof port === "number") ? parseInt(port) : options.port;
 
-  //Check the port value
-  port = (typeof port === 'number') ? parseInt(port) : options.port;
+    //Get the static files folder path
+    options.folder = path.resolve(process.cwd(), options.folder);
 
-  //Fix the static files folder path
-  options.folder = path.resolve(process.cwd(), options.folder);
+    //Get the error file path
+    options.error = path.resolve(process.cwd(), options.error);
 
-  //Fix the error file path
-  options.error = path.resolve(process.cwd(), options.error);
+    //Initialize the server
+    let server = http.createServer(function (req, res) {
+        let timeStart = Date.now();
 
-  //Initialize the server
-  var server = http.createServer(function(req, res)
-  {
-    //Time for make the request
-    var time_start  = Date.now();
+        //Check the cors option
+        if (options.cors === true) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+            res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+        }
+        let pathname = url.parse(req.url).pathname;
+        let localPath = path.join(options.folder, pathname);
+        if (path.extname(localPath) === "") {
+            //Add the index file to the local path
+            localPath = path.join(localPath, "./" + path.basename(options.index));
+        }
 
-    //Check the cors option
-    if(options.cors === true)
-    {
-      //Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', '*');
+        //Reponse finish event
+        res.on("finish", function () {
+            console.log("" + res.statusCode + "  " + pathname + "  " + (Date.now() - timeStart) + " ms");
+        });
 
-      //Request methods you wish to allow
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        //Check if the file exists in this directory
+        return utily.fs.isFile(localPath, function (error, exists) {
+            if (error) {
+                return errorPage(res, 500, "Error processing your request.");
+            }
+            if (exists === false) {
+                return errorPage(res, 404, "File not found.");
+            }
 
-      //Request headers you wish to allow
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    }
+            //Write the header with the content type
+            res.writeHead(200, {"Content-Type": mime.getType(localPath)});
 
-    //Parse the request url and get only the pathname
-    var pathname = url.parse(req.url).pathname;
-
-    //Resolve to the local folder
-    var local_path = path.join(options.folder, pathname);
-
-    //Check the extension
-    if(path.extname(local_path) === '')
-    {
-      //Add the index file to the local path
-      local_path = path.join(local_path, './' + path.basename(options.index));
-    }
-
-    //Reponse finish event
-    res.on('finish', function()
-    {
-      //Output message
-      var message = [];
-
-      //Add the response code
-      message[0] = (res.statusCode >= 300) ? colors.red(res.statusCode) : colors.green(res.statusCode);
-
-      //Add the response path
-      message[1] = colors.blue(pathname);
-
-      //Add the total time in ms
-      message[2] = colors.white((Date.now() - time_start) + ' ms');
-
-      //Display the log message
-      console.log('' + message.join('  ') + '');
+            //Initialize the reader stream
+            //let reader = fs.createReadStream(local_path, { encoding: "utf8" });
+            //Remove encoding -> fixed bug reading images (jpg, png, etc...)
+            let reader = fs.createReadStream(localPath);
+            reader.on("data", function (data) {
+                //Write the data to the response
+                res.write(data);
+            });
+            reader.on("end", function () {
+                res.end("");
+            });
+            reader.on("error", function (error) {
+                return errorPage(res, 500, "Something went wrong...");
+            })
+        });
     });
 
-    //Check if the file exists in this directory
-    return utily.fs.isFile(local_path, function(error, exists)
-    {
-      //Check the error
-      if(error){ return error_page(res, 500, 'Error processing your request.'); }
-
-      //Check if file does not exists
-      if(exists === false){ return error_page(res, 404, 'File not found.'); }
-
-      //Write the header with the content type
-      res.writeHead(200, { 'Content-Type': mime.getType(local_path) });
-
-      //Initialize the reader stream
-      //var reader = fs.createReadStream(local_path, { encoding: 'utf8' });
-      //Remove encoding -> fixed bug reading images (jpg, png, etc...)
-      var reader = fs.createReadStream(local_path);
-
-      //Reader data
-      reader.on('data', function(data)
-      {
-        //Write the data to the response
-        res.write(data);
-      });
-
-      //No more data -> End event
-      reader.on('end', function()
-      {
-        //End the write
-        res.end('');
-      });
-
-      //Error reading file
-      reader.on('error', function(error)
-      {
-        //Render the error page
-        return error_page(res, 500, 'Something went wrong...');
-      })
+    //Start server
+    server.listen(port, function () {
+        if (typeof cb === "function") {
+            cb.call(null);
+        }
+        else {
+            //Show the console log success
+            console.log("");
+            console.log("Static server listening on: " + "http://localhost:" + options.port + "");
+            console.log("Reading files from: " + options.folder + "");
+            console.log("");
+        }
     });
-  });
-
-  //Start server
-  server.listen(port, function()
-  {
-    //Check if the callback method is a function
-    if(typeof cb === 'function')
-    {
-      //Call the callback function
-      cb.call(null);
-    }
-    else
-    {
-      //Show the console log success
-      console.log('');
-      console.log('Static server listening on: ' + colors.blue('http://localhost:' + options.port) + '');
-      console.log('Reading files from: ' + colors.blue(options.folder) + '');
-      console.log('');
-    }
-  });
 };
 
 //Display an error page
-var error_page = function(res, error_code, error_message)
-{
-  //Write the header with the content type
-  res.writeHead(error_code, { 'Content-Type': 'text/html' });
-
-  //Read the error file path
-  return fs.readFile(options.error, 'utf8', function(error, data)
-  {
-    //Check the error
-    if(error){ return res.end('<h1>Error</h1><p>' + error_message + '</p>'); }
-
-    //Replace the html file
-    data = utily.string.format(data, { code: error_code, message: error_message });
-
-    //Return the error page
-    return res.end(data);
-  });
+let errorPage = function (res, errorCode, errorMessage) {
+    res.writeHead(errorCode, {"Content-Type": "text/html"});
+    //Read the local error file
+    return fs.readFile(options.error, "utf8", function (error, data) {
+        //Check the error
+        if (error) {
+            return res.end("<" + "h1>Error</h1><p>" + errorMessage + "</p>");
+        }
+        data = utily.string.format(data, {code: errorCode, message: errorMessage});
+        return res.end(data);
+    });
 };
